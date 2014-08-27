6f78388b4c13fb27acd90e17547a0352f5654c83
Patrick Yates <frozen.dinosaur@gmail.com>
2014-08-27 23:43:06 +1000

    Initial commit

---
+++ BeatDetection/OnsetDetector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BeatDetection
+{
+    public abstract class OnsetDetector
+    {
+        public List<float> Beats = new List<float>();
+        public AudioWrapper Audio { get; set; }
+
+        public OnsetDetector(AudioWrapper a)
+        {
+            Audio = a;
+        }
+
+        public abstract void DetectBeats();
+    }
+}
---
+++ BeatDetection/QMVampWrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace BeatDetection
+{
+    class QMVampWrapper : OnsetDetector
+    {
+        string beats;
+        float correctionAmount = 0;
+        public QMVampWrapper(AudioWrapper a, string f, float correction) : base(a)
+        {
+            beats = f;
+            correctionAmount = correction;
+        }
+
+        public override void DetectBeats()
+        {
+            using (StreamReader sr = new StreamReader(beats))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Beats.Add(float.Parse(line.Split(',')[0]) + correctionAmount);
+                }
+                sr.Close();
+            }
+        }
+    }
+}
