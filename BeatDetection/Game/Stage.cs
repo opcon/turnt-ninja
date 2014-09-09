0f86a07c84fa04b4ff710126dbd812c542ad0a8f
Patrick Yates <frozen.dinosaur@gmail.com>
2014-09-09 23:22:29 +1000

    initial implementation of refactoring the stage code into its own class

--- BeatDetection/Game/Stage.cs
+++ BeatDetection/Game/Stage.cs
@@ -1,22 +1,79 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using BeatDetection.Core;
+using NAudio.Wave;
+using Substructio.Core;
+using Wav2Flac;
 
 namespace BeatDetection.Game
 {
     class Stage
     {
-        private double _TotalTime;
-        private float[] _Beats;
-        private List<PolarPolygon> _Polygons; 
+        private double _totalTime;
+        private PolarPolygon[] _polygons;
+        private QMVampWrapper _qmVampWrapper;
+
+        private WaveOut _waveOut;
+        private IWaveProvider _waveProvider;
+
+        private Random _random;
+        private int _hashCode;
+        private const int HashCount = 10000;
 
         public Stage()
         {
 
         }
 
+        public void Load(string audioPath, string sonicPath, string pluginPath, float correction)
+        {
+            LoadAudioStream(audioPath);
+            LoadAudioFeatures(audioPath, sonicPath, pluginPath, correction);
+        }
+
+        private void LoadAudioStream(string audioPath)
+        {
+            byte[] hashBytes = new byte[HashCount];
+
+            if (Path.GetExtension(audioPath).Equals(".flac", StringComparison.CurrentCultureIgnoreCase))
+            {
+                var str = new MemoryStream();
+                var output = new WavWriter(str);
+                var fr = new FlacReader(audioPath, output);
+                fr.Process();
+                str.Position = 0;
+                str.Read(hashBytes, 0, HashCount);
+                str.Position = 0;
+
+                var fmt = new WaveFormat(fr.inputSampleRate, fr.inputBitDepth, fr.inputChannels);
+                var s = new RawSourceWaveStream(str, fmt);
+                _waveProvider = s;
+                _waveOut.Init(_waveProvider);
+            }
+            else
+            {
+
+                var audioReader = new AudioFileReader(audioPath);
+                audioReader.Read(hashBytes, 0, HashCount);
+                audioReader.Position = 0;
+                _waveProvider = audioReader;
+                _waveOut.Init(_waveProvider);
+            }
+
+            _hashCode = CRC16.Instance().ComputeChecksum(hashBytes);
+            _random = new Random(_hashCode);
+        }
+
+        private void LoadAudioFeatures(string audioPath, string sonicPath, string pluginPath, float correction)
+        {
+            _qmVampWrapper = new QMVampWrapper(audioPath, sonicPath, pluginPath, correction);
+            _qmVampWrapper.DetectBeats();
+        }
+
         public void Update(double time)
         {
-            _TotalTime += time;
+            _totalTime += time;
         }
 
         public void Draw(double time)
