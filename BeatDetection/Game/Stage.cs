892733d92bf19d73315c7180d2c446f5cfd8bb41
Patrick Yates <frozen.dinosaur@gmail.com>
2014-09-10 10:19:37 +1000

    fully implement game functions into stage class - can now be used

--- BeatDetection/Game/Stage.cs
+++ BeatDetection/Game/Stage.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
+using System.Linq;
 using BeatDetection.Core;
 using NAudio.Wave;
+using OpenTK;
+using OpenTK.Input;
 using Substructio.Core;
 using Wav2Flac;
 
@@ -13,6 +17,8 @@ namespace BeatDetection.Game
         private double _totalTime;
         private PolarPolygon[] _polygons;
         private QMVampWrapper _qmVampWrapper;
+        private int _polygonIndex;
+        private int _polygonsToRemoveCount;
 
         private WaveOut _waveOut;
         private IWaveProvider _waveProvider;
@@ -20,6 +26,10 @@ namespace BeatDetection.Game
         private Random _random;
         private int _hashCode;
         private const int HashCount = 10000;
+
+        private Player _player;
+        private PolarPolygon _centerPolygon;
+        private int _direction;
 
         public Stage()
         {
@@ -30,11 +40,15 @@ namespace BeatDetection.Game
         {
             LoadAudioStream(audioPath);
             LoadAudioFeatures(audioPath, sonicPath, pluginPath, correction);
+
+            _player = new Player();
+            _centerPolygon = new PolarPolygon(6, 6, 0, 1, 0, 80);
         }
 
         private void LoadAudioStream(string audioPath)
         {
             byte[] hashBytes = new byte[HashCount];
+            _waveOut = new WaveOut();
 
             if (Path.GetExtension(audioPath).Equals(".flac", StringComparison.CurrentCultureIgnoreCase))
             {
@@ -69,16 +83,116 @@ namespace BeatDetection.Game
         {
             _qmVampWrapper = new QMVampWrapper(audioPath, sonicPath, pluginPath, correction);
             _qmVampWrapper.DetectBeats();
+
+            _polygons = new PolarPolygon[_qmVampWrapper.Beats.Count];
+
+            var angles = new double[6];
+            for (int i = 0; i < 6; i++)
+            {
+                angles[i] = MathHelper.DegreesToRadians((i + 1) * (60));
+            }
+
+            int prevStart = _random.Next(5);
+            double prevTime = 0;
+            int c = 0;
+
+            int index = 0;
+
+            var sorted = _qmVampWrapper.Beats.OrderBy(f => f);
+            foreach (var b in sorted) 
+            {
+                var col = Color.White;
+                int start = 0;
+                if (b - prevTime < 0.2)
+                {
+                    c++;
+                    start = prevStart;
+                    col = Color.Red;
+                }
+                else if (b - prevTime < 0.4)
+                {
+                    start = (prevStart + 6) + _random.Next(0, 2) - 1;
+                }
+                else
+                {
+                    start = _random.Next(5);
+                    c = 0;
+                }
+
+                _polygons[index] = new PolarPolygon(6, 5, b, 400, angles[start % 6], 125);
+
+                //for (int i = 0; i < 5; i++)
+                //{
+                //    //hexagons.Add(new PolarPolygon(b, 300) { theta = angles[start] + angles[((i+start)%6)] });
+                //    hexagonSides.Add(new PolarPolygonSide(b, 400, angles[start % 6] + i * angles[0], 125) { Colour = col });
+                //}
+                prevTime = b;
+                prevStart = start;
+                //hexagons.Add(new PolarPolygon(b, 300) { theta = angles[0] });
+                //hexagons.Add(new PolarPolygon(b, 300) { theta = angles[1] });
+                //hexagons.Add(new PolarPolygon(b, 300) { theta = angles[2] });
+                //hexagons.Add(new PolarPolygon(b, 300) { theta = angles[3] });
+
+                index++;
+            }
+
         }
 
         public void Update(double time)
         {
-            _totalTime += time;
+
+            if (_waveOut.PlaybackState != PlaybackState.Playing)
+            {
+                _waveOut.Play();
+                time = 0;
+            }
+            if (_waveOut.PlaybackState == PlaybackState.Playing)
+            {
+                _totalTime += time;
+                _player.Direction = _direction;
+                _centerPolygon.Direction = _direction;
+                _player.Update(time);
+
+                _polygonIndex += _polygonsToRemoveCount;
+                _polygonsToRemoveCount = 0;
+
+                for (int i = _polygonIndex; i < _polygons.Length; i++)
+                {
+                    var poly = _polygons[i];
+                    poly.Direction = _direction;
+                    poly.Update(time);
+                    if (poly.Destroy)
+                        _polygonsToRemoveCount++;
+                    else if ((poly.Radius - poly.ImpactDistance)/(poly.Speed) < (poly.PulseWidthMax/poly.PulseMultiplier))
+                        _centerPolygon.Pulsing = true;
+                }
+
+                if (_polygonsToRemoveCount > 0)
+                {
+                    var d = _random.NextDouble();
+                    _direction = d > 0.95 ? -_direction : _direction;
+                   // _centerPolygon.Pulse(time);
+                }
+
+                _centerPolygon.Update(time, false);
+                //if (InputSystem.CurrentKeys.Contains(Key.Left))
+                //    _centerPolygon.Rotate(time);
+                //else if (InputSystem.CurrentKeys.Contains(Key.Right))
+                //    _centerPolygon.Rotate(-time * 1.5);
+
+            }
         }
 
         public void Draw(double time)
         {
-            
+            for (int i = _polygonIndex; i < _polygons.Length; i++)
+            {
+                _polygons[i].Draw(time);
+            }
+
+            _centerPolygon.Draw(time);
+            _player.Draw(time);
+
         }
     }
 }
