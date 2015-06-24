8fd4f60ca81d1aaca7cd395df1431e7af2995862
Patrick Yates <frozen.dinosaur@gmail.com>
2015-06-24 22:49:20 +1000

    Add MaxVolume property to StageAudio Add IsStopped property to StageAudio Tweak FadeOut method to use Task.Run instead of Task.StartNew and await Task.Delay instead of Thread.Sleep Implemented FadeIn method - similar to FadeOut - could potentially combine the two Add CancellationTokens to both FadeOut and FadeIn so that these methods can be cancelled Added CancelAudioFades method to cancel the audio fade tasks Implemented IDisposable for StageAudio to dispose _waveOut and _waveProvider

--- BeatDetection/Game/StageAudio.cs
+++ BeatDetection/Game/StageAudio.cs
@@ -10,20 +10,37 @@ using Wav2Flac;
 
 namespace BeatDetection.Game
 {
-    class StageAudio
+    class StageAudio : IDisposable
     {
         public int AudioHashCode { get; private set; }
         private const int HashCount = 10000;
         private WaveOut _waveOut;
         private WaveStream _waveProvider;
+        private float _maxVolume;
+
+        private CancellationTokenSource _tokenSource = new CancellationTokenSource();
 
         /// <summary>
-        /// Max volume is 1.0f
+        /// Volume is clamped between 0 and MaxVolume
         /// </summary>
         public float Volume
         {
             get { return _waveOut.Volume; }
             set { _waveOut.Volume = MathHelper.Clamp(value, 0, 1); }
+        }
+
+        /// <summary>
+        /// Max volume is between 0 and 1
+        /// </summary>
+        public float MaxVolume
+        {
+            get { return _maxVolume; }
+            set { _maxVolume = MathHelper.Clamp(value, 0, 1); }
+        }
+
+        public bool IsStopped
+        {
+            get { return _waveOut.PlaybackState == PlaybackState.Stopped; }
         }
 
         public void Load(string audioPath)
@@ -94,25 +111,72 @@ namespace BeatDetection.Game
         /// <param name="time"></param>
         /// <param name="minVolume"></param>
         /// <param name="dVolume"></param>
-        /// <param name="PauseOrStop">0 for nothing, 1 for pause, 2 for stop</param>
-        public void FadeOut(float time, float minVolume, float dVolume, int PauseOrStop)
+        /// <param name="pauseOrStop">0 for nothing, 1 for pause, 2 for stop</param>
+        public void FadeOut(float time, float minVolume, float dVolume, int pauseOrStop)
         {
-            Task.Factory.StartNew(() =>
+            var ct = _tokenSource.Token;
+            Task.Run(async () =>
             {
-                int dt = (int) (time/((Volume - minVolume)/dVolume));
-                while (Volume > minVolume)
+                int dt = (int)(time / ((Volume - minVolume) / dVolume));
+                while (Volume > minVolume && !ct.IsCancellationRequested)
                 {
                     Volume -= dVolume;
-                    Thread.Sleep(dt);
+                    await Task.Delay(dt, ct);
                 }
-                if (PauseOrStop == 1) Pause();
-                else if (PauseOrStop == 2) Stop();
-            });
+                switch (pauseOrStop)
+                {
+                    case 1:
+                        Pause();
+                        break;
+                    case 2:
+                        Stop();
+                        break;
+                }
+            }, ct);
         }
 
-        public void FadeIn(float time, float maxVolume, float dVolume)
+        /// <summary>
+        /// Fades in the audio
+        /// </summary>
+        /// <param name="time">Time over which to fade the audio in</param>
+        /// <param name="maxVolume">The maximum volume to reach</param>
+        /// <param name="dVolume">The volume step size to use</param>
+        /// <param name="pauseOrStop">0 for nothing, 1 to pause after the fade in, 2 to stop after the fade in</param>
+        public void FadeIn(float time, float maxVolume, float dVolume, int pauseOrStop)
         {
-            
+            if (maxVolume > MaxVolume) throw new ArgumentOutOfRangeException("The maximum fade in volume " + maxVolume + " was greater than the maximum volume for this audio " + MaxVolume);
+            var ct = _tokenSource.Token;
+            Task.Run(async () =>
+            {
+                int dt = (int)(time / ((maxVolume - Volume) / dVolume));
+                while (Volume < maxVolume && !ct.IsCancellationRequested)
+                {
+                    Volume += dVolume;
+                    await Task.Delay(dt, ct);
+                }
+                switch (pauseOrStop)
+                {
+                    case 1:
+                        Pause();
+                        break;
+                    case 2:
+                        Stop();
+                        break;
+                }
+            }, ct);
+        }
+
+        public void CancelAudioFades()
+        {
+            _tokenSource.Cancel();
+            _tokenSource = new CancellationTokenSource();
+        }
+
+        public void Dispose()
+        {
+            _waveOut.Dispose();
+            //may error here if _waveOut disposes _waveProvider?
+            _waveProvider.Dispose();
         }
     }
 }
