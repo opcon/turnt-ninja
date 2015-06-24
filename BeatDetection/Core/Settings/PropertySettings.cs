5b21f61d38967ca984a06dfe5d79c6ea0bad2ac7
Patrick Yates <frozen.dinosaur@gmail.com>
2015-06-24 22:41:59 +1000

    Remove Directories class (superseded by DirectoryHandler) Add a GameSettings implementation that uses Application properties

---
+++ BeatDetection/Core/Settings/PropertySettings.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading;
+using Gwen.Control;
+using Substructio.Core.Settings;
+
+namespace BeatDetection.Core.Settings
+{
+    class PropertySettings : IGameSettings
+    {
+        private bool _loaded;
+        private Dictionary<string, SettingsPropertyValue> _settings;
+
+        public object this[string key]
+        {
+            get
+            {
+                if (!_loaded) throw new Exception("GameSettings was not loaded before access");
+                if (_settings.ContainsKey(key)) return _settings[key].PropertyValue;
+                throw new GameSettingNotFoundException("The game setting with key {0} was not found", key);
+            }
+            set { if (_settings.ContainsKey(key)) _settings[key].PropertyValue = value; }
+        }
+
+        public void Save()
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        public void Load()
+        {
+            Properties.Settings.Default.Reload();
+            var antiAliasingSamples = Properties.Settings.Default.AntiAliasingSamples;
+            antiAliasingSamples *= 2;
+            //load all the settings
+            _settings = new Dictionary<string, SettingsPropertyValue>();
+            SettingsPropertyValue[] values = new SettingsPropertyValue[Properties.Settings.Default.PropertyValues.Count];
+            Properties.Settings.Default.PropertyValues.CopyTo(values, 0);
+            foreach (SettingsPropertyValue propertyValue in values)
+            {
+                _settings.Add(propertyValue.Name, propertyValue);
+            }
+            _loaded = true;
+        }
+    }
+
+    public class GameSettingNotFoundException : Exception
+    {
+        public GameSettingNotFoundException() : base()
+        {
+        }
+
+        public GameSettingNotFoundException(string message) : base(message)
+        {
+        }
+
+        public GameSettingNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public GameSettingNotFoundException(string format, params object[] args) : base(string.Format(format, args))
+        {
+        }
+    }
+}
