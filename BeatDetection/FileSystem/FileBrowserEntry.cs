ba0218f9ee8df643e2a76d34daa31d4d488aed42
Patrick Yates <frozen.dinosaur@gmail.com>
2016-05-05 12:38:27 +1000

    Fixed bug in filesystem entry type, where HasFlag(Song) would return true if either Song or Directory

--- BeatDetection/FileSystem/FileBrowserEntry.cs
+++ BeatDetection/FileSystem/FileBrowserEntry.cs
@@ -16,11 +16,11 @@ namespace BeatDetection.FileSystem
     [Flags]
     public enum FileBrowserEntryType
     {
-        Song = 0,
-        Directory = 1 << 0,
-        Drive = 1 << 1,
-        Special = 1 << 2,
-        Separator = 1 << 3,
-        Plugin = 1 << 4
+        Song = 1 << 0,
+        Directory = 1 << 1,
+        Drive = 1 << 2,
+        Special = 1 << 3,
+        Separator = 1 << 4,
+        Plugin = 1 << 5
     }
 }
