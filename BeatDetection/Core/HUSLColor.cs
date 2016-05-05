19d04ff28bea714d1a6be7e21878408f7eb59007
Patrick Yates <frozen.dinosaur@gmail.com>
2016-05-05 12:37:01 +1000

    Add commented out code for trying new colour generation methods

--- BeatDetection/Core/HUSLColor.cs
+++ BeatDetection/Core/HUSLColor.cs
@@ -20,7 +20,12 @@ namespace BeatDetection.Core
 
         public HUSLColor(Color4 color)
         {
-            var res = HUSL.ColorConverter.RGBToHUSL(new List<double>{color.R, color.G, color.B});
+            //var c = new ColorMine.ColorSpaces.Rgb { R = color.R*255, G = color.G*255, B = color.B*255 };
+            //var r = c.To<ColorMine.ColorSpaces.Hsl>();
+            //H = r.H;
+            //S = r.S;
+            //L = r.L;
+            var res = HUSL.ColorConverter.RGBToHUSL(new List<double> { color.R, color.G, color.B });
             H = res[0];
             S = res[1];
             L = res[2];
@@ -33,6 +38,9 @@ namespace BeatDetection.Core
 
         public static Color4 ToColor4(HUSLColor color)
         {
+            //var c = new ColorMine.ColorSpaces.Hsl { H = color.H, L = color.L, S = color.S };
+            //var r = c.ToRgb();
+            //return new Color4((float)r.R/255, (float)r.G/255, (float)r.B/255, 1.0f);
             var res = HUSL.ColorConverter.HUSLToRGB(new List<double>{ color.H, color.S, color.L });
             return new Color4((byte)((res[0]) * 255), (byte)((res[1]) * 255), (byte)((res[2]) * 255), 255);
         }
