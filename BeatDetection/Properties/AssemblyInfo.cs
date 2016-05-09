e1ba9670ef8f1425c958bf63042a878ef4baba48
Patrick Yates <frozen.dinosaur@gmail.com>
2016-05-09 22:56:20 +1000

    Update description and copyright year

--- BeatDetection/Properties/AssemblyInfo.cs
+++ BeatDetection/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Turnt Ninja")]
-[assembly: AssemblyDescription("Get turnt")]
+[assembly: AssemblyDescription("Turnt Ninja")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Opcon")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
