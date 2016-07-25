59d3cf813f664431a40d259fc3c2ebeaf49edff0
Patrick Yates <frozen.dinosaur@gmail.com>
2016-07-25 10:06:26 +1000

    Added Piwik and Sentry implementations
    For IAnalytics and IErrorReporting respectively

---
+++ src/TurntNinja/Logging/PiwikAnalytics.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Substructio.Logging;
+using Substructio.Core;
+using Piwik.Tracker;
+
+namespace TurntNinja.Logging
+{
+    class PiwikAnalytics : IAnalytics
+    {
+        readonly string _piwikURL;
+        readonly int _appID;
+        readonly Platform _platform;
+        readonly string _platformVersion;
+        readonly int _resX;
+        readonly int _resY;
+        readonly string _gameVersion;
+        readonly string _userID;
+        readonly string _baseURL;
+        readonly Uri _baseUri;
+        readonly string _userAgent;
+
+        private PiwikTracker _piwikTracker;
+
+        public PiwikAnalytics(int appID, string piwikURL, Platform platform, string platformVersion, int resX, int resY, string gameVersion, string userID, string baseURL)
+        {
+            _piwikURL = piwikURL;
+            _appID = appID;
+            _platform = platform;
+            _platformVersion = platformVersion;
+            _resX = resX;
+            _resY = resY;
+            _gameVersion = gameVersion;
+            _userID = userID;
+            _baseURL = baseURL;
+            _baseUri = new Uri(_baseURL);
+
+            // Initialize tracking client
+            _piwikTracker = new PiwikTracker(_appID, _piwikURL);
+
+            // Cookies don't exist without an HTTP Context,
+            // but disable them explicitly for peace of mind
+            _piwikTracker.disableCookieSupport();
+
+            // Set piwik timeout
+            _piwikTracker.setRequestTimeout(10);
+
+            // Set user agent
+            _userAgent = BuildUserAgent();
+            _piwikTracker.setUserAgent(_userAgent);
+
+            // Set resolution
+            _piwikTracker.setResolution(_resX, _resY);
+
+            // Set anonymous user id
+            _piwikTracker.setUserId(_userID);
+
+            // Force new visit on first tracking call, since we've just
+            // started the application, i.e. a visit
+            _piwikTracker.setForceNewVisit();
+
+            // Set default base url
+            _piwikTracker.setUrl(_baseURL);
+        }
+
+        public void TrackApplicationStartup()
+        {
+            TrackApplicationView("startup", "Application Startup");
+        }
+
+        public void TrackApplicationShutdown()
+        {
+            TrackApplicationView("shutdown", "Application Shutdown");
+        }
+
+        public void TrackEvent(string eventCategory, string eventAction, string eventSubjectName = "", string eventValue = "")
+        {
+            // Set game version custom dimension for all tracking requests
+            _piwikTracker.setCustomTrackingParameter("dimension1", _gameVersion);
+
+            // Track the event
+            _piwikTracker.doTrackEvent(eventCategory, eventAction, eventSubjectName, eventValue);
+        }
+
+        public void TrackApplicationView(string relativeURL, string title="")
+        {
+            // Set game version custom dimension for all tracking requests
+            _piwikTracker.setCustomTrackingParameter("dimension1", _gameVersion);
+
+            // Set page "url"
+            _piwikTracker.setUrl(new Uri(_baseUri, relativeURL).ToString());
+
+            // Track the page view
+            _piwikTracker.doTrackPageView(title);
+        }
+
+        public void SetCustomVariable(int variableID, string variableName, string variableValue, CustomVariableScope variableScope)
+        {
+            var scope = (variableScope == CustomVariableScope.ApplicationLaunch) ? CustomVar.Scopes.visit : CustomVar.Scopes.page;
+            _piwikTracker.setCustomVariable(variableID, variableName, variableValue, scope);
+        }
+
+        private string BuildUserAgent()
+        {
+            var os = "";
+            switch (_platform)
+            {
+                case Platform.Windows:
+                    os = "Windows";
+                    break;
+                case Platform.Linux:
+                    os = "Linux";
+                    break;
+                case Platform.MacOSX:
+                    os = "Mac";
+                    break;
+            }
+            return $"{os} {_platformVersion}";
+        }
+
+    }
+}
---
+++ src/TurntNinja/Logging/SentryErrorReporting.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Substructio.Logging;
+using SharpRaven;
+using SharpRaven.Data;
+using Substructio.Core;
+
+namespace TurntNinja.Logging
+{
+    class SentryErrorReporting : IErrorReporting
+    {
+        readonly Dsn _sentryDsn;
+        readonly string _userGUID;
+
+        RavenClient _sentryClient;
+
+        public SentryErrorReporting(string sentryURL, string environment, string version, string userGUID, Platform platform, string platformVersion, bool scrubUserName = false)
+        {
+            _sentryDsn = new Dsn(sentryURL);
+            _userGUID = userGUID;
+            InitialiseSentryClient();
+
+            _sentryClient.Environment = environment;
+            _sentryClient.Release = version;
+            _sentryClient.Tags["OS"] = $"{platform} {platformVersion}";
+
+            if (scrubUserName) _sentryClient.LogScrubber = new SentryUserScrubber();
+        }
+
+        private void InitialiseSentryClient()
+        {
+            _sentryClient = new RavenClient(_sentryDsn, null, null, new SentryUserGUIDFactory(_userGUID));
+        }
+
+        public void ReportError(Exception ex)
+        {
+            var sentryEvent = new SentryEvent(ex);
+            _sentryClient.Capture(sentryEvent);
+        }
+
+        public void ReportMessage(string message)
+        {
+            var sentryMessage = new SentryMessage(message);
+            var sentryEvent = new SentryEvent(sentryMessage);
+            _sentryClient.Capture(sentryEvent);
+        }
+    }
+
+    class SentryUserGUIDFactory : SentryUserFactory
+    {
+        readonly string _username;
+
+        public SentryUserGUIDFactory(string username)
+        {
+            _username = username;
+        }
+        protected override SentryUser OnCreate(SentryUser user)
+        {
+            return new SentryUser(_username);
+        }
+    }
+
+    class SentryUserScrubber : SharpRaven.Logging.IScrubber
+    {
+        public string Scrub(string input)
+        {
+            var search = "user\":{";
+            var start = input.IndexOf(search, StringComparison.Ordinal) + search.Length;
+            var end = input.IndexOf("}", start, StringComparison.Ordinal);
+            var ret = input.Substring(0, start) + input.Substring(end, input.Length - end);
+            return ret;
+        }
+    }
+}
