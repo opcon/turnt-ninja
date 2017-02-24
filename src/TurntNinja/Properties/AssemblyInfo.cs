231c95824d688484d154b7c1f4087f22530c8689
Patrick Yates <frozen.dinosaur@gmail.com>
2017-02-25 10:42:22 +1100

    Update assembly info

--- src/TurntNinja/Properties/AssemblyInfo.cs
+++ src/TurntNinja/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.14.*")]
-[assembly: AssemblyInformationalVersion("Tag:0.14 Head:develop Sha:43ef62")]
+[assembly: AssemblyInformationalVersion("")]
